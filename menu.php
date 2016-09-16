ff153f8c1fed9d73bcf4674621b5d50d47a57378
Muhammad Aldi Rizki Ramadhan <ezziiiiii@gmail.com>
2016-09-16 18:30:39 +0700

    hapus line
    hapus line yang bikin eror

--- menu.php
+++ menu.php
@@ -58,106 +58,53 @@
                     <button type="submit" class="btn btn-default">Submit</button>
                   </form>
                   <ul class="nav navbar-nav navbar-right">
-<<<<<<< HEAD
-                <li><a href="#" id="myBtn"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a>
-                    <!-- Modal -->
-              <div class="modal fade" id="myModal" role="dialog">
-                <div class="modal-dialog">
-                    <!-- Modal content-->
-                    <div class="modal-content">
-                        <div class="modal-header">
-                          <button type="button" class="close" data-dismiss="modal">&times;</button>
-                          <h3><a href="#"><i class="glyphicon glyphicon-user"></i></a> Sign in!</h3>
-                        </div>
-                        <div class="modal-body">
-                          <form action="#" method="post" class="form" role="form">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <input type="email" class="form-control" name="email" placeholder="Enter Email">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <input type="password" class="form-control" name="password"
-                                    placeholder="Enter Password">
-                                </div>
-                            </div>
-                            <button class="btn btn-lg btn-default btn-block" type="submit">
-                                Sign in</button>
-                          </form>
-                        </div>
-                        <div class="modal-footer">
-                          <button type="submit" class="btn btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-                          <p>Not a member? <a href="#">Sign Up</a></p>
-                          <p>Forgot <a href="#">Password?</a></p>
-                        </div>
-                    </div>
-                </div>
-              </div>
-              <script>
-                $(document).ready(function(){
-                    $("#myBtn").click(function(){
-                        $("#myModal").modal();
-                    });
-                });
-              </script> 
-                </li>
-                <li><a href="shoppingcart.html" data-toggle="tooltip" data-placement="bottom" title="Your Shopping Cart">
-                  <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
-                </li>
-              </ul>
-=======
                     <li><a href="#" id="myBtn"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></a>
                         <!-- Modal -->
-                      <div class="modal fade" id="myModal" role="dialog">
-                        <div class="modal-dialog">
-                          <!-- Modal content-->
-                          <div class="modal-content">
-                              <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h3><a href="#"><span class="glyphicon glyphicon-user"></span></a> Sign in!</h3>
-                              </div>
-                              <div class="modal-body">
-                                <form action="#" method="post" class="form" role="form">
-                                  <div class="row">
-                                      <div class="col-md-12">
-                                        <div class="form-group">
-                                          <label for="email">Email address:</label>
-                                          <input type="email" class="form-control" id="email">
-                                        </div>
-                                        <div class="form-group">
-                                          <label for="pwd">Password:</label>
-                                          <input type="password" class="form-control" id="pwd">
-                                        </div>
-                                        <div class="checkbox">
-                                          <label><input type="checkbox"> Remember me</label>
-                                        </div>
-                                        <button type="submit" class="btn btn-default btn-block btn-lg">Sign in</button>
-                                      </div>
+                        <div class="modal fade" id="myModal" role="dialog">
+                          <div class="modal-dialog">
+                              <!-- Modal content-->
+                              <div class="modal-content">
+                                  <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h3><a href="#"><i class="glyphicon glyphicon-user"></i></a> Sign in!</h3>
                                   </div>
-                                </form>
-                              </div>
-                              <div class="modal-footer">
-                                <button type="submit" class="btn btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-                                <p>Not a member? <a href="signup.php">Sign Up</a></p>
-                                <p>Forgot <a href="#">Password?</a></p>
+                                  <div class="modal-body">
+                                    <form action="#" method="post" class="form" role="form">
+                                      <div class="row">
+                                          <div class="col-md-12">
+                                              <input type="email" class="form-control" name="email" placeholder="Enter Email">
+                                          </div>
+                                      </div>
+                                      <div class="row">
+                                          <div class="col-md-12">
+                                              <input type="password" class="form-control" name="password"
+                                              placeholder="Enter Password">
+                                          </div>
+                                      </div>
+                                      <button class="btn btn-lg btn-default btn-block" type="submit">
+                                          Sign in</button>
+                                    </form>
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="submit" class="btn btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+                                    <p>Not a member? <a href="#">Sign Up</a></p>
+                                    <p>Forgot <a href="#">Password?</a></p>
+                                  </div>
                               </div>
                           </div>
                         </div>
-                      </div>
-                      <script>
-                        $(document).ready(function(){
-                            $("#myBtn").click(function(){
-                                $("#myModal").modal();
-                            });
-                        });
-                      </script> 
+                        <script>
+                          $(document).ready(function(){
+                              $("#myBtn").click(function(){
+                                  $("#myModal").modal();
+                              });
+                          });
+                        </script> 
                     </li>
                     <li><a href="shoppingcart.html" data-toggle="tooltip" data-placement="bottom" title="Your Shopping Cart">
                       <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
                     </li>
                   </ul>
->>>>>>> master
                 </div><!-- /.navbar-collapse -->
               </div><!-- /.container-fluid -->
             </nav>
