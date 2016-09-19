6e1639b66fa511e9319c2d932ebe2936a029c28e
Muhammad Aldi Rizki Ramadhan <ezziiiiii@gmail.com>
2016-09-19 14:01:44 +0700

    bikin pop up untuk signup
    bikin signup jadi pop up

--- menu.php
+++ menu.php
@@ -1,3 +1,55 @@
+<?php
+
+session_start();
+
+if(isset($_SESSION['sys_id'])) {
+    header("Location: index.php");
+    }
+
+    include_once ("config_mb/configure_mb.php");
+
+//set validation error flag as false
+$error = false;
+
+//check if form is submitted
+if (isset($_POST['signup'])) {
+    $firstname = mysqli_real_escape_string($dbc, $_POST['firstname']);
+    $lastname = mysqli_real_escape_string($dbc, $_POST['lastname']);
+    $email = mysqli_real_escape_string($dbc, $_POST['email']);
+    $password = mysqli_real_escape_string($dbc, $_POST['password']);
+    $repassword = mysqli_real_escape_string($dbc, $_POST['repassword']);
+
+
+    //name can contain only alpha characters and space
+    if (!preg_match("/^[a-zA-Z ]+$/",$firstname)) {
+        $error = true;
+        $name_error = "Name must contain only alphabets and space";
+    }
+    if(!filter_var($email,FILTER_VALIDATE_EMAIL)) {
+        $error = true;
+        $email_error = "Please Enter Valid Email ID";
+    }
+    if(strlen($password) < 6) {
+        $error = true;
+        $password_error = "Password must be minimum of 6 characters";
+    }
+    if($password != $repassword) {
+        $error = true;
+        $repassword_error = "Password and Confirm Password doesn't match";
+    }
+    if (!$error) {
+        if(mysqli_query($dbc, "INSERT INTO sys_signup(firstname,lastname,email,password) VALUES('" . $firstname . "','" . $lastname . "', '" . $email . "', '" . md5($password) . "')")) {
+            $successmsg = "Successfully Registered! <a href='login.php'> Click here to Login</a>";
+        } 
+        else 
+        {
+            $errormsg = "Error in registering...Please try again later!";
+        }
+    }
+}
+    
+?>
+
 <div class="menu container">
     <div class="col-md-2">
             <a href="/web_cttproject/"><img src="/web_cttproject/images/logo.png"></a>
@@ -90,7 +142,82 @@
                               </div>
                               <div class="modal-footer">
                                 <button type="submit" class="btn btn-default pull-left" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-                                <p>Not a member? <a href="signup.php">Sign Up</a></p>
+                                <p>Not a member? <a href="#" id="myBtn2">Sign Up</a>
+                                  <!-- Modal -->
+                                  <div class="modal fade" id="myModal2" role="dialog">
+                                    <div class="modal-dialog">
+                                      <!-- Modal content-->
+                                      <div class="modal-content">
+                                          <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                            <h3><a href="#"><span class="glyphicon glyphicon-user"></span></a> Sign up!</h3>
+                                          </div>
+                                          <div class="modal-body">
+                                            <div class="row">
+                                              <div class="col-md-12">
+                                                  <form action="#" method="post" class="form" role="form">
+                                                  <div class="row">
+                                                      <div class="col-md-6 col-sm-6">
+                                                          <input class="form-control" name="firstname" placeholder="First Name" type="text"
+                                                              required value="<?php if($error) echo $firstname; ?>"> 
+
+                                                              <span class="text-danger"><?php if (isset($name_error)) echo $name_error; ?></span>
+                                                      </div>
+                                                      <div class="col-md-6 col-sm-6">
+                                                          <input class="form-control" name="lastname" placeholder="Last Name" type="text" required />
+                                                      </div>
+                                                  </div>
+                                                  <input class="form-control" name="email" placeholder="Your Email" type="email" required value="<?php if($error) echo $email; ?>"/>
+                                                  <span class="text-danger"><?php if (isset($email_error)) echo $email_error; ?></span>
+
+                                                  <input class="form-control" name="password" placeholder="New Password" type="password" />
+                                                  <span class="text-danger"><?php if (isset($password_error)) echo $password_error; ?></span>
+                                                  <input class="form-control" name="repassword" placeholder="Re-Password" type="password" />
+                                                  <span class="text-danger"><?php if (isset($repassword_error)) echo $repassword_error; ?></span>
+                                                  <label for="">
+                                                      Birth Date</label>
+                                                  <div class="row">
+                                                      <div class="col-md-4 col-sm-4">
+                                                          <input class="form-control"  type="datetime" id="example1" placeholder="dd/mm/yyyy">
+                                                      </div>
+                                                      
+                                                  </div>
+                                                  <label class="radio-inline">
+                                                      <input type="radio" name="sex" id="inlineCheckbox1" value="male" />
+                                                      Male
+                                                  </label>
+                                                  <label class="radio-inline">
+                                                      <input type="radio" name="sex" id="inlineCheckbox2" value="female" />
+                                                      Female
+                                                  </label>
+                                                  <br />
+                                                  <br />
+                                                  <button class="btn btn-lg btn-primary btn-block" value="Sign Up" name="signup"   type="submit">
+                                                      Sign up</button>
+                                                  </form>
+                                                  <span class="text-success"><?php if (isset($successmsg)) { echo $successmsg; } ?></span>
+                                                  <span class="text-danger"><?php if (isset($errormsg)) { echo $errormsg; } ?></span>
+                                              </div>
+                                            </div>
+                                          </div>
+                                          <div class="modal-footer">
+                                            <div class="row">
+                                              <div class="col-md-4 col-md-offset-4 text-center">    
+                                              Already Registered? <a href="login.php">Login Here</a>
+                                              </div>
+                                            </div>
+                                          </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                  <script>
+                                    $(document).ready(function(){
+                                        $("#myBtn2").click(function(){
+                                            $("#myModal2").modal();
+                                        });
+                                    });
+                                  </script>
+                                </p>
                                 <p>Forgot <a href="#">Password?</a></p>
                               </div>
                           </div>
@@ -104,7 +231,7 @@
                         });
                       </script> 
                     </li>
-                    <li><a href="shoppingcart.html" data-toggle="tooltip" data-placement="bottom" title="Your Shopping Cart">
+                    <li><a href="shopingcart.html" data-toggle="tooltip" data-placement="bottom" title="Your Shopping Cart">
                       <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a>
                     </li>
                   </ul>
