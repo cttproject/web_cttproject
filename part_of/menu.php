eb7db0a03031744e7a5b00d6ddbcf155799c0291
Diastowo Faryduana <diastowo@gmail.com>
2016-10-04 23:24:57 +0700

    header menu

--- part_of/menu.php
+++ part_of/menu.php
@@ -1,6 +1,6 @@
 <div class="menu container-fluid">
-    <div class="col-md-2"></div>
-    <div class="col-md-8">
+    <!--<div class="col-md-2"></div>-->
+    <div class="col-md-12">
         <nav class="navbar navbar-default">
             <div class="container-fluid">
                 <!-- Brand and toggle get grouped for better mobile display -->
@@ -14,22 +14,24 @@
                 </div>
 
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <div class="col-md-8 collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
                         <li><a href="index.php?home">All News <span class="sr-only">(current)</span></a></li>
                         <li><a href="index.php?hot_news">Hot News</a></li>
                         <li><a href="index.php?old_news">Old News</a></li>
                     </ul>
-                    <form class="navbar-form navbar-right">
+
+                </div><!-- /.navbar-collapse -->
+                <div class="col-md-4 navbar-form navbar-right">
+                    <form class="">
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Search">
                         </div>
                         <button type="submit" class="btn btn-default">Search</button>
                     </form>
-
-                </div><!-- /.navbar-collapse -->
+                </div>
             </div><!-- /.container-fluid -->
         </nav>
     </div>
-    <div class="col-md-2"></div>
+    <!--<div class="col-md-2"></div>-->
 </div><!--end of menu-->
