f1acda1506c0ce31bc4141ef77efcc69aab37940
Muhammad Aldi Rizki Ramadhan <ezziiiiii@gmail.com>
2016-10-15 14:39:40 +0700

    old news > trending topic

--- part_of/list_group.php
+++ part_of/list_group.php
@@ -1,24 +1,31 @@
 <div class="selection">
     <div class="list-group">
         <p class="list-group-item active">
-            Old News
+            Trending Topic
         </p>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Pad</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Acc</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
-        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>
+        <?php
+        $query = "select * from sys_news";
+        $result = mysqli_query($dbc, $query);
+
+        while ($nav = mysqli_fetch_assoc($result)) { ?>
+            <a href="#" class="list-group-item"><?php echo $nav['judul'] ?></a>
+        <?php }
+        ?><!--end of new update-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Pad</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Acc</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
+<!--        <a href="#" class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</a>-->
     </div>
 </div>
